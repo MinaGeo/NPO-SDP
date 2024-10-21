8f744b5156d79b568b1ce7487ee34fd4f4da8db2
MinaGeo <20P4198@eng.asu.edu.eg>
2024-10-21 17:51:54 +0300

    First Create

---
+++ main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Hello World</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        $txt = "Hello World";
+        $version = phpversion();
+        echo "$txt from PHP version $version";
+        ?>
+    </h1>
+</body>
+
+</html>
