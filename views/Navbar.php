4fa5d823a7207dcd8fd4c02a379c8b29be96b2d2
Ahmed Wael <87478304+ahmed-wael2002@users.noreply.github.com>
2025-01-14 23:36:48 +0200

    Updated Navbar with Donations Admin

--- views/Navbar.php
+++ views/Navbar.php
@@ -31,25 +31,28 @@
             <img class="brand-logo" src="/assets/rotaract.png" alt="community club logo">
         </a>-->
         <ul id="nav-mobile" class="left hide-on-med-and-down">
-    <li><a href="home">Home</a></li>
-    <li><a href="donation">Donation</a></li>
-    <li><a href="event">Events</a></li>
-    <?php if ($_SESSION['USER_TYPE'] == 0): ?>
-        <li><a class="dropdown-trigger" href="#!" data-target="adminDropdown">Shop<i class="material-icons right"></i></a></li>
-        <!-- Dropdown Structure -->
-        <ul id="adminDropdown" class="dropdown-content">
-            <li><a href="shop">Shop</a></li>
-            <li><a href="showCategoryTree">Show Category Tree</a></li>
+            <li><a href="home">Home</a></li>
+            <li><a href="donation">Donation</a></li>
+            <li><a href="event">Events</a></li>
+            <?php if ($_SESSION['USER_TYPE'] == 0): ?>
+                <li><a class="dropdown-trigger" href="#!" data-target="adminDropdown">Shop<i class="material-icons right"></i></a></li>
+                <!-- Dropdown Structure -->
+                <ul id="adminDropdown" class="dropdown-content">
+                    <li><a href="shop">Shop</a></li>
+                    <li><a href="showCategoryTree">Show Category Tree</a></li>
+                </ul>
+            <?php elseif ($_SESSION['USER_TYPE'] == 1): ?>
+                <li><a href="shop">Shop</a></li>
+            <?php endif; ?>
         </ul>
-    <?php elseif ($_SESSION['USER_TYPE'] == 1): ?>
-        <li><a href="shop">Shop</a></li>
-    <?php endif; ?>
-</ul>
 
         <ul id="nav-mobile" class="right hide-on-med-and-down">
             <?php if ($_SESSION['USER_ID'] != -1): ?>
                 <!-- Dropdown Trigger -->
                 <li><a href="cartHistory">Cart History</a></li>
+                <?php if ($_SESSION['USER_ID'] == 1): ?>
+                    <li><a href="donationAdmin">Donations Admin</a></li>
+                <?php endif; ?>
                 <li><a href="updateUser">Welcome, <?php echo htmlspecialchars($_SESSION['USERNAME']); ?></a></li>
                 <li><a href="logout">Logout</a></li>
             <?php else: ?>
